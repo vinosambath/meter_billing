e4e065d99c3751ac7a2d0265a81756284589b2e6
Vinoth Kumar Sambath <vinsam@Vinoths-MacBook-Air.local>
2024-09-18 10:37:41 +0800

    Add sharded db creation queries to mysql_changes_audit

--- src/db/mysql_changes_audit.sql
+++ src/db/mysql_changes_audit.sql
@@ -1,5 +1,9 @@
 create user 'vinxsam'@'localhost' identified by 'Password@1';
 
+create database readings_01;
+create database readings_02;
+
+--> in both readings_01 db and readings_02 db
 create table meter_readings (
     id varchar(36) not null,
     `nmi` varchar(10) not null,
@@ -8,3 +12,4 @@ create table meter_readings (
     constraint meter_readings_pk primary key (id),
     constraint meter_readings_unique_consumption unique (`nmi`, `timestamp`)
 );
+
